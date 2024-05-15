742afb4601dc88c367ac31dd773d1b3449f9f0ff
RH-YANG <mago1291@naver.com>
2024-05-15 19:17:03 +0900

    commit

--- 1_basic/f_CSS적용/1_css-module/src/Button.js
+++ 1_basic/f_CSS적용/1_css-module/src/Button.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import style from './Button.module.css';
-
-function Button({ size }) {
-    if (size === 'big') {
-        return <button className={`${style.button} ${style.big}`}>큰 버튼</button>;
-    } else {
-        return <button className={`${style.button} ${style.small}`}>작은 버튼</button>;
-    }
-}
-export default Button;
-console.log(style);
-
-
+// 일반 버전
 // import React from 'react';
 // import style from './Button.module.css';
-// import cn from 'classnames';
 
 // function Button({ size }) {
 //     if (size === 'big') {
-//         return <button className={cn(style.button, style.big)}>큰 버튼</button>;
+//         return <button className={`${style.button} ${style.big}`}>큰 버튼</button>;
 //     } else {
-//         return <button className={cn(style.button, style.small)}>작은 버튼</button>;
+//         return <button className={`${style.button} ${style.small}`}>작은 버튼</button>;
 //     }
 // }
-// console.log(style);
 // export default Button;
+
+
+
+// classnames 패키지 사용한 버전
+import React from 'react';
+import style from './Button.module.css';
+import cn from 'classnames';
+
+function Button({ size }) {
+    if (size === 'big') {
+        return <button className={cn(style.button, style.big)}>큰 버튼</button>;
+    } else {
+        return <button className={cn(style.button, style.small)}>작은 버튼</button>;
+    }
+}
+console.log(style);
+export default Button;
